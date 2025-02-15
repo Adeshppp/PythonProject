bfb6445d75d0229cd4f0802a7a7af8ddad69eb56
Adesh <adeshpadwal21@gmail.com>
2025-02-14 21:00:33 -0500

    adding completed task functionality

--- ToDoListFlaskProject/app.py
+++ ToDoListFlaskProject/app.py
@@ -15,6 +15,8 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), unique=False,nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_completed = db.Column(db.DateTime)
+    is_deleted = db.Column(db.Boolean, default=False)
     def __repr__(self):
         return '<Task %r>' % self.id
     
@@ -34,8 +36,23 @@ def index():
         return render_template('index.html', tasks=tasks)
 
 
-@app.route('/delete/<int:id>')
+@app.route('/complete/<int:id>')
 def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    print("task_to_delete ",task_to_delete)
+    task_to_delete.is_deleted = True
+    task_to_delete.date_completed = datetime.utcnow()
+    print("after")
+    print("task_to_delete.is_deleted ",task_to_delete.is_deleted)
+    print("task_to_delete.date_completed ",task_to_delete.date_completed)
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error deleting the task.'
+    
+@app.route('/deletePermanent/<int:id>')
+def deletePermanent(id):
     task_to_delete = Todo.query.get_or_404(id)
     
     try:
@@ -43,7 +60,7 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was an error deleting the task.'
+        return 'There was an error deleting the task permanently.'
 
 
 @app.route('/update/<int:id>', methods=['POST','GET'])
@@ -59,6 +76,19 @@ def update(id):
             return 'There was an error updating the task.'
     else:
         return render_template('update.html',task=task_to_update)
+
+
+@app.route('/review/<int:id>', methods=['GET'])
+def reviewTask(id):
+    task_to_review = Todo.query.get_or_404(id)
+    task_to_review.is_deleted = False
+    task_to_review.date_completed = None  # reset the completion date to None when reviewing the task again
+    print("task_to_review.date_completed",task_to_review.date_completed)
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error updating the task.'
 
 if __name__ == "__main__":
     app.run(debug=True)
