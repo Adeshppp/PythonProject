98eace2e16a50afb2ae103ce8de142c590999dc5
Adesh <adeshpadwal21@gmail.com>
2025-02-15 20:52:46 -0500

    refractored html page by using jinja syntax and added table

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -8,3 +8,11 @@ def home_page():
     return render_template("home.html")
 
 
+@app.route('/market')
+def market_page():
+    items = [
+        {"id": 1, "name": "Laptop", "barcode": "123456789012", "price": 999.99},
+        {"id": 2, "name": "Smartphone", "barcode": "987654321098", "price": 699.99},
+        {"id": 3, "name": "Headphones", "barcode": "567890123456", "price": 199.99}
+    ]
+    return render_template("market.html", items = items)
